4f697b11b4a9e0a6afa13c60d0f2215895906fb7
aaronst <aaronst@uw.edu>
2014-04-30 15:48:54 -0700

    Main method

--- src/HW3.java
+++ src/HW3.java
@@ -2,4 +2,7 @@
  * Created by Aaron on 4/30/2014.
  */
 public class HW3 {
+    public static void main(String[] args) {
+        System.out.println("Aaron Stephens & Chad Condon - Homework 3");
+    }
 }

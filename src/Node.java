163d29f4a628894cc4f7d3256cd669035d7e06ae
Chad Condon <chad.condon@gmail.com>
2014-04-30 17:49:50 -0700

    x

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,45 @@
 /**
- * Created by Aaron on 4/30/2014.
+ * A node for the LRL binary tree structure.
+ * Each node represents a (sub)expression within an LRL program.
+ * 
+ * @custom.assign 
+ * @author Chad Condon
+ * @author Aaron Stephens
+ * @version 0.1
+ * 
+ * @custom.instruct John Mayer, Ph.D.
+ * @custom.course TCSS 342 Data Structures Spring 2014
+ * @custom.due 2014-05-07
  */
 public class Node {
+    
+    /**
+     * The data contained in the <code>Node</code>. This must represent
+     * either an operator, a variable, or an integer. 
+     */
+    private String data;
+    
+    /**
+     * The first operand of the expression.
+     * May be <code>null</code> for integers or variables.
+     */
+    private Node left;
+    
+    /**
+     * The second operand of the expression.
+     * May be <code>null</code> for integers, variables, or print operators.
+     */
+    private Node right;
+    
+    public Node(String data) {
+        this.data = data;
+    }
+    
+    protected void setLeft(Node left) {
+        this.left = left;
+    }
+    
+    protected void setRight(Node right) {
+        this.right = right;
+    }
 }
